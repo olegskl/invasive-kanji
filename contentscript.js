5e08b8157f9b14ada295378dc0244ba6f17d34f1
olegskl <sklyanchuk@gmail.com>
2012-12-28 21:37:46 +0100

    First overhaul intended to fix glaring bugs. Wait for pages to come in focus before triggering questions. Avoid triggering questions on pages built with framesets. Add a possibility to provide multiple answers to the same question. Fix horizontal alignment of the answer field.

--- contentscript.js
+++ contentscript.js
@@ -1,21 +1,35 @@
 /*jslint browser: true */
-/*globals kanjilist */
+/*globals kanjilist  */
 
 'use strict';
 
-var redirectionURL = 'http://jisho.org/kanji/details/%s',
-    entryIndex = Math.floor(Math.random() * kanjilist.length),
-    entry = kanjilist[entryIndex],
-    timer,
+var redirectionURL = 'http://jisho.org/kanji/details/{q}',
     coverElement = document.createElement('div'),
     answerElement,
-    kanji = entry.kanji,
-    html = '<div id="extension-invasive-kanji-question">' + kanji + '</div>' +
+    docVisibilityChange = 'visibilitychange',
+    docHidden = 'hidden',
+    template = '<div id="extension-invasive-kanji-question">{q}</div>' +
         '<input type="text" id="extension-invasive-kanji-answer" placeholder=' +
         '"type the meaning...">';
 
-function isValidAnswer(answer) {
-    return entry.meanings.indexOf(answer) !== -1;
+// Document Hidden API vendor prefixes, urghhh...
+if (document.hidden === undefined) {
+    docVisibilityChange = 'webkitvisibilitychange';
+    docHidden = 'webkitHidden';
+}
+
+function parseAnswer(answer) {
+    return answer.trim().split(/\s*\,\s*/);
+}
+
+function areCorrectAnswers(userAnswers, correctAnswers) {
+    return userAnswers.every(function (userAnswer) {
+        return correctAnswers.indexOf(userAnswer) !== -1;
+    });
+}
+
+function removeCover() {
+    coverElement.parentNode.removeChild(coverElement);
 }
 
 function continueToPage() {
@@ -23,44 +37,69 @@ function continueToPage() {
     coverElement.style.opacity = '0';
     // Listen to the animation events and when the animation ends, remove the
     // cover element to give access to the underlying content:
-    coverElement.addEventListener('webkitTransitionEnd', function () {
-        coverElement.parentNode.removeChild(coverElement);
-    }, true);
+    coverElement.addEventListener('webkitTransitionEnd', removeCover);
 }
 
 function performRedirect(queryTerm) {
-    window.location = redirectionURL.replace('%s', queryTerm);
+    window.location = redirectionURL.replace('{q}', queryTerm);
 }
 
-coverElement.id = 'extension-invasive-kanji-cover';
-coverElement.innerHTML = html;
+function askQuestion(question, correctAnswers) {
+    var timer;
 
-document.body.appendChild(coverElement);
+    // Avoid troubles with framesets by working with body only:
+    if (document.body.nodeName !== 'BODY') { return; }
 
-answerElement = document.getElementById('extension-invasive-kanji-answer');
-answerElement.focus();
+    coverElement.id = 'extension-invasive-kanji-cover';
+    coverElement.innerHTML = template.replace('{q}', question);
 
-answerElement.addEventListener('keypress', function (e) {
-    // Waiting for the "enter" key:
-    if (e.which === 13) {
+    document.body.appendChild(coverElement);
+
+    answerElement = document.getElementById('extension-invasive-kanji-answer');
+    answerElement.focus();
+
+    answerElement.addEventListener('keypress', function (e) {
+        // Only wait for the "enter" key to be pressed:
+        if (e.which !== 13) { return; }
         // The timer is no longer required:
         clearTimeout(timer);
         // Take action based on the validity of the answer:
-        if (isValidAnswer(this.value)) {
+        if (areCorrectAnswers(parseAnswer(this.value), correctAnswers)) {
             continueToPage();
         } else {
-            performRedirect(entry.kanji);
+            performRedirect(question);
         }
-    }
-});
+    });
 
-// Trigger CSS animation by changing the opacity setting:
-// (note that we should )
-setTimeout(function () {
-    coverElement.style.opacity = '1';
-}, 0);
+    setTimeout(function () {
+        // Trigger CSS animation by changing the opacity setting:
+        coverElement.style.opacity = '1';
+    }, 0);
 
-// Create motivation by enforcing a time limit:
-timer = setTimeout(function () {
-    performRedirect(entry.kanji);
-}, 10000);
+    // Motivate user by enforcing a time limit:
+    timer = setTimeout(function () {
+        performRedirect(question);
+    }, 10000);
+}
+
+function askRandomQuestion() {
+    // Randomize the question:
+    var entryIndex = Math.floor(Math.random() * kanjilist.length),
+        entry = kanjilist[entryIndex];
+    askQuestion(entry.kanji, entry.meanings);
+}
+
+function docVisibilityHandle() {
+    // Don't do anything until the document is visible:
+    if (document[docHidden]) { return; }
+    // No need to ask again on the next document visibility change:
+    document.removeEventListener(docVisibilityChange, docVisibilityHandle);
+    // It is now safe to ask the question:
+    askRandomQuestion();
+}
+
+if (document[docHidden]) {
+    document.addEventListener(docVisibilityChange, docVisibilityHandle);
+} else {
+    askRandomQuestion();
+}
